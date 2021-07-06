ec089b2225027b9c51cb03fda8e4697b86d8ce58
Raven <ravenkoehler@gmail.com>
2021-07-06 11:08:19 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,7 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.all.select {|student| student.match?(/#{params[:query]}/)}
   end
 
   def student_params
