767ad7bd5e9ede6db4c6f7add604520b5b68037e
Raven <ravenkoehler@gmail.com>
2021-07-13 13:58:56 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(criteria)
+    if criteria.present?
+      where("NAME like ?", "%#{criteria}%")
+    else
+      self.all
+    end
+  end
 end
